d4917dc47d16103fb1fbb8cd6cbeab660e821be2
artur <brugeman.artur@gmail.com>
2024-07-05 14:10:55 +0200

    Added site files

---
+++ sw.js
@@ -0,0 +1,4 @@
+
+    importScripts("https://cdn.npubpro.com/index.js");
+    self.nostrSite.startSW([{ url: "https://cdn.npubpro.com/index.js", revision: "1720180783301" }, { url: "/nostr-site-demo/sw.js", revision: "1720180783302" }, { url: "/nostr-site-demo/manifest.webmanifest", revision: "1720180783303" }]);
+  
